843cb5d2cd39017d16271320e6f23fed594acb12
ericclemmons <eric@smarterspam.com>
2010-12-21 11:50:10 -0800

    Made app.php the only front controller that uses APPLICATION_ENV for environment settings

--- web/app.php
+++ web/app.php
@@ -4,5 +4,11 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('prod', false);
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', getenv('APPLICATION_ENV') ?: 'production');
+
+$debug = (APPLICATION_ENV !== 'production');
+
+$kernel = new AppKernel(APPLICATION_ENV, $debug);
 $kernel->handle(new Request())->send();
